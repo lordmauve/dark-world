c18a913016c1b64f424a27076c79fece4e9247f7
Daniel Pope <mauve@mauveweb.co.uk>
2018-04-21 11:01:34 +0100

    Free teleports for developers

---
+++ manage.py
@@ -0,0 +1,39 @@
+import click
+
+from darkworld.persistence import pickle_atomic, load_pickle
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('username')
+@click.argument('capability')
+def grant(username, capability):
+    pickfile = f'{username}-caps.pck'
+    caps = load_pickle(pickfile)
+    if not caps:
+        click.echo(f'No such user {username}', err=True)
+        return
+
+    caps.add(capability)
+    pickle_atomic(pickfile, caps)
+
+
+@cli.command()
+@click.argument('username')
+def show(username):
+    pickfile = f'{username}-caps.pck'
+    caps = load_pickle(pickfile)
+    if not caps:
+        click.echo(f'No such user {username}', err=True)
+        return
+
+    for c in sorted(caps):
+        print(c)
+
+
+if __name__ == '__main__':
+    cli()
